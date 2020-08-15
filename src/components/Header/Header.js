ab8e9ec5894764d0176488fb6c21e712de0b8fa8
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-15 02:19:59 +0100

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import AcUnitRoundedIcon from '@material-ui/icons/AcUnitRounded'; 
+import { useStyles } from './styles';
 
 const Header = () => {
-return(
-    <AppBar>
-        <Toolbar>
-            <Typography>
-                This is the Header
-            </Typography>
-        </Toolbar>
+    const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography>This is the Header</Typography>
+        <AcUnitRoundedIcon />
+      </Toolbar>
     </AppBar>
-)
-}
+  );
+};
 
-export default Header
+export default Header;

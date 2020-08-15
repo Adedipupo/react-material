a99c0fe35fcc7a0136454ca587fa9860f20a1a6a
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-15 02:51:53 +0100

    card

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,13 +5,16 @@ import { Grid } from '@material-ui/core';
 const Content = () => {
 return(
    <Grid container spacing={2}>
-      <Grid item xs={4}>
+      <Grid item xs={12} sm={3}>
          <SimpleCard />
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={12} sm={3}>
          <SimpleCard />
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={12} sm={3}>
+         <SimpleCard />
+      </Grid>
+      <Grid item xs={12} sm={3}>
          <SimpleCard />
       </Grid>
    </Grid>

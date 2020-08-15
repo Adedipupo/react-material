32735170cd7029217035b3096f5ff18f56657947
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-15 02:03:22 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Grid } from '@material-ui/core'
+
 
 function App() {
   return (
-    <div>
-      <h1>California</h1>
-    </div>
+    <Grid container direction="column">
+      <Grid item>Navigation</Grid>
+      <Grid item container>
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+        Content
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
+    </Grid>
   );
 }
 

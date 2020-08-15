7ce66774f761f76b8714e691ff9f6cef4f1d7cde
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-15 02:10:13 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
 import { Grid } from '@material-ui/core'
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <Grid container direction="column">
-      <Grid item>Navigation</Grid>
+      <Grid item>
+        <Header />
+      </Grid>
       <Grid item container>
         <Grid item xs={0} sm={2} />
         <Grid item xs={12} sm={8}>

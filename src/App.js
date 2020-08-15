97bd03e1847f690867de9d84500c7d43270248a4
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-15 02:07:02 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <Grid container direction="column">
       <Grid item>Navigation</Grid>
       <Grid item container>
-        <Grid item xs={2} />
-        <Grid item xs={8}>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>
           ContentContentContent
           ContentContentContent
                     ContentContentContent
@@ -17,7 +17,7 @@ function App() {
                     ContentContentContent
           ContentContentContent
         </Grid>
-        <Grid item xs={2} />
+        <Grid item xs={0} sm={2} />
       </Grid>
     </Grid>
   );
